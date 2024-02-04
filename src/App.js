ae5b16571effdefc84dc6bc6fbc93ee2dc81581d
moulikuntrapaku <moulikuntrapaku@gmail.com>
2024-02-04 21:08:35 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import DetailPage from './DetailPage';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route path="/item/:id" element={<DetailPage/>} />
+      </Routes>
+  </>
 }
 
 export default App;
